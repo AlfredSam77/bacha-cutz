32c10019e25639f05cbec8ac8253ee1fc9ef759f
promise samuel <promisealfredsamuel73781637-la@gomycode.tn>
2026-02-05 15:25:37 +0100

    bacha website

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function bookNow() {
+  window.open(
+       "https://wa.me/2348117819518?text=Hello%20Bacha%20Cutz,%20%0AI%20would%20like%20to%20book%20an%20appointment.%0A%0AName:%0AService:%0ADate:%0ATime:",
+      "_blank"
+  );
+}
+
+
+// function bookNow() {
+// alert('Thank you for choosing OrangeCut! Please call us to book your appointment.');
+// }
